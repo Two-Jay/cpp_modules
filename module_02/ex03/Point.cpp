02ae877081cefab99d122cfc638351fd8bdfdbe7
KIM JEONGJUN <kimjeongjun@KIMs-MacBook-Pro.local>
2022-02-22 14:49:30 +0900

    commit

--- module_02/ex03/Point.cpp
+++ module_02/ex03/Point.cpp
@@ -6,35 +6,37 @@
 /*   By: jekim <jekim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 23:47:28 by jekim             #+#    #+#             */
-/*   Updated: 2022/02/20 11:12:46 by jekim            ###   ########.fr       */
+/*   Updated: 2022/02/22 14:40:15 by jekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 #include "Fixed.hpp"
+#include <iostream>
 
 Point::Point() : x(0), y(0) {}
 Point::~Point() {}
-Point::Point(const Fixed& x, const Fixed& y) : x(x), y(y) { }
-Point::Point(const Point& p) { *this = p; }
+Point::Point(const Fixed &x, const Fixed &y) : x(x), y(y) {}
+Point::Point(const Point &p) : x(p.get_Fixed_x()), y(p.get_Fixed_y()) {}
 
 Fixed const Point::get_Fixed_x(void) const { return this->x; }
 Fixed const Point::get_Fixed_y(void) const { return this->y; }
 
-Point& Point::operator= (const Point& p)
+Point &Point::operator=(const Point &p)
 {
-    (Fixed)this->x = p.get_Fixed_x();
-    (Fixed)this->y = p.get_Fixed_y();
+    if (this != &p)
+    {
+    }
     return *this;
 }
 
-std::ostream& operator<< (std::ostream& os, Point& p)
+std::ostream &operator<<(std::ostream &os, Point &p)
 {
     os << "[x: " << p.get_Fixed_x() << " y: " << p.get_Fixed_y() << "]";
     return os;
 }
 
-std::ostream& operator<< (std::ostream& os, const Point& p)
+std::ostream &operator<<(std::ostream &os, const Point &p)
 {
     os << "[x: " << p.get_Fixed_x() << " y: " << p.get_Fixed_y() << "]";
     return os;
