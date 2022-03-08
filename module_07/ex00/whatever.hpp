38d03baf6ca5763a7ce261b1bd2f407a68d3ee56
KIM JEONGJUN <kimjeongjun@KIMs-MacBook-Pro.local>
2022-03-08 22:05:08 +0900

    checked

--- module_07/ex00/whatever.hpp
+++ module_07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: jekim <jekim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 01:52:58 by jekim             #+#    #+#             */
-/*   Updated: 2022/02/28 02:12:46 by jekim            ###   ########.fr       */
+/*   Updated: 2022/03/08 22:04:51 by jekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@ void swap(T &a, T &b)
 
 template <typename T>
 T min(T a, T b) {
-    return a > b ? b : a;
+    return a >= b ? b : a;
 }
 
 template <typename T>
 T max(T a, T b) {
-    return a > b ? a : b;
+    return a <= b ? b : a;
 }
 
 #endif //WHATEVER_HPP
